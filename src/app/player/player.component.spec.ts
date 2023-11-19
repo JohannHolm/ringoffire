0ee7d960b5f62a90b59da4563b37c187af379a5d
JohannHolm <johannholm94@gmx.de>
2023-11-19 19:30:32 +0100

    player-bar

--- src/app/player/player.component.spec.ts
+++ src/app/player/player.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { PlayerComponent } from '../player/player.component';
-import { GameComponent } from './game.component';
 
-describe('GameComponent', () => {
-  let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+import { PlayerComponent } from './player.component';
+
+describe('PlayerComponent', () => {
+  let component: PlayerComponent;
+  let fixture: ComponentFixture<PlayerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameComponent],
+      imports: [PlayerComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(GameComponent);
+    fixture = TestBed.createComponent(PlayerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = [];
+  public players: string[] = ['Johann', 'Ermi', 'Kotfresse'];
   public stack: string[] = [];
   public playerCards: string[] = [];
   public currentPlayer: number = 0;
